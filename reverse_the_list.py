8e3811afd8d726806a8eefa163b208e95c8c896c
Saravanan.M <46522992+saravananmahalingam@users.noreply.github.com>
2019-02-18 19:40:45 +0530

    Update reverse_the_list.py

--- reverse_the_list.py
+++ reverse_the_list.py
@@ -3,8 +3,8 @@ a=int(input())
 b=list(map(int,input().split()))
 b.reverse()
 c=len(b)
-for i in b:
-	if i==b[c-1]:
-		print(i,end="")
+for i in range(c):
+	if i==c-1:
+		print(b[i],end="")
 	else:
-		print(i,end="->")
+		print(b[i],end="->")
